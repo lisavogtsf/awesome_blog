ea568939fbbbdbad9c2464df83ee033891d9b632
Mark Centoni <marklc44@gmail.com>
2014-08-23 12:57:37 -0700

    user presence, length validations working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, :email,
+            :presence => true,
+            :length => {:minimum => 2}
 
+  # validates_format_of :email,
+  #           :with => /A(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Z‌​a-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$\z/i
 end
