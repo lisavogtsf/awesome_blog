a248abaebe3b312557fdfcd8195b83e550c28b61
Mark Centoni <marklc44@gmail.com>
2014-08-23 15:52:45 -0700

    rename post body column and to content

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823223617) do
+ActiveRecord::Schema.define(version: 20140823224723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.text     "body"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
