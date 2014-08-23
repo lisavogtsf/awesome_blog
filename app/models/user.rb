5c2525bfbd59cb3dab8815c8dcb39c5f8b4c1764
Mark Centoni <marklc44@gmail.com>
2014-08-23 16:06:03 -0700

    update email regex to account for dots before and after at sym

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => true
 
   validates_format_of :email,
-            :with => /\A[\w-]+@[\w-]+\.[a-z]{2,3}\z/i
+            :with => /\A([\w-]|\.)+@([\w-]|\.)+\.[a-z]{2,3}\z/i
 end
