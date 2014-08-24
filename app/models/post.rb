5cac6fe9e4594b8d53e813d86fc877286f6c7f35
Mark Centoni <marklc44@gmail.com>
2014-08-24 15:20:21 -0700

    add tags, post_tags and setup relationship with post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :post_tags
+  has_many :tags, :through => :post_tags
 
   validates :title, :content, :user_id,
             :presence => true
