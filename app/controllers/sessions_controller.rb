5ede954340fa67ce48c5ed1060d8d3105066c457
Rakulon <kekknyx@gmail.com>
2014-08-25 16:53:08 -0700

    Sessions built

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+	def new
+  end
+
+  def create
+
+	@user = User.authenticate(params[:user][:email],params[:user][:password])
+
+	 if @user
+	 	session[:user_id] = @user.id
+	    	render text: 'Logged in!'
+	   	 else 
+	    	render text: "I have no idea who you are."	
+	 end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	render text: "Logged out."
+  end
+end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,7 +1,9 @@
 class SitesController < ApplicationController
 
   # site home page
+  before_action :is_authenticated?
   def index
-
+  	 @user = User.find_by(id: session[:user_id])
+  	
   end
 end
