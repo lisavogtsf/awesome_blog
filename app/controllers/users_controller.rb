deab400eac4b998ad949edebb95be8262667401a
Lisa Vogt <lisavogt.sf@gmail.com>
2014-08-24 20:12:53 -0700

    adds show route to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,42 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
+	def index
+		@users = User.all
+	end
 
-  # display new user form
-  def new
-    @user = User.new
-  end
+	# display new user form
+	def new
+		@user = User.new
+	end
 
-  # create new user
-  def create
+	# create new user
+	def create
 
-  end
+	end
 
-  # edit the user
-  def edit
-    @user = find_user_by_id
-  end
+	# edit the user
+	def edit
+		@user = find_user_by_id
+	end
 
-  # udate the user
-  def update
-  end
+	# udate the user
+	def update
+	end
 
-  # show one user
-  def show
-    find_user_by_id
-  end
+	# show one user
+	def show
+		@user = User.find_by_id(params[:id])
+	end
 
-  # delete the user
-  def destroy
-  end
+	# delete the user
+	def destroy
+	end
 
-  private
+	private
 
-  def find_user_by_id
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
-  end
+	def find_user_by_id
+		user_id = params[:id]
+		@user = User.find_by_id(user_id)
+	end
 
 end
