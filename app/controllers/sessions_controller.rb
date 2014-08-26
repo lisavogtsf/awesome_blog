28953155113622d6ee7e8cbcb13eec9777378935
Lisa Vogt <lisavogt.sf@gmail.com>
2014-08-25 18:50:18 -0700

    corrects login and logout routes, go to users pages and home respectively

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
 	 if @user
 	 	session[:user_id] = @user.id
-	    	render text: 'Logged in!'
+	    	redirect_to users_path
 	   	 else 
 	    	render text: "I have no idea who you are."	
 	 end
