575f456f24ec1c7c1bfd7562a3852e9b247f8634
Mark Centoni <marklc44@gmail.com>
2014-08-26 08:44:57 -0700

    move sessions to new branch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,9 @@ class SessionsController < ApplicationController
 
 	 if @user
 	 	session[:user_id] = @user.id
-	    	redirect_to users_path
-	   	 else
-	    	render text: "I have no idea who you are."
+    redirect_to users_path
+	else
+	 render text: "I have no idea who you are."
 	 end
   end
 
