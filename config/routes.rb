32f09d4b7411ab17cb88e08adebdad4df3c18941
JesseMoore <vergobret@gmail.com>
2014-08-27 19:01:24 -0700

    Fixed route conflict

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,8 @@ AwesomeBlog::Application.routes.draw do
     resources :pages
   end
 
-  resources :posts do
-    resources :comments, only: [:create]
-  end
-
   post "posts/:post_id/comments", to: "comments#create", :as => "post_comments"
-  post "posts/:post_id/comments/:id", to: "comments#create", :as => "post_comment"
+  # post "posts/:post_id/comments/:id", to: "comments#create", :as => "post_comment"
   # post "comments/:id/comment", to: "comments#create", :as => "comment_comments"
 
   get '/login' => "sessions#new"
