0eabf9e1fbb3ef35653f134834b474a953dfc39e
indotek68 <angga.w.putra@gmail.com>
2014-08-28 11:39:47 -0700

    fixed body scrolling

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,7 +3,10 @@ class SitesController < ApplicationController
   before_action :is_authenticated?
   before_filter :is_authenticated?, :except => :index
   # site home page
+
+
   def index
+  	@body_class = 'home-body'
   	 @user = User.find_by(id: session[:user_id])
      if current_user != nil
       redirect_to users_path
